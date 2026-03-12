c44f7aaf2883ab1fd083dc6575c7c38132f9fd5b
jahnavikoneti <jahnavikoneti0021@gmail.com>
2026-03-13 00:58:26 +0530

    cors erroe

--- src/main/java/com/jahnavi/contribution/config/CorsConfig.java
+++ src/main/java/com/jahnavi/contribution/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000","https://your-app.vercel.app"
+        config.setAllowedOrigins(List.of("http://localhost:3000","https://contribution-frontend-jahnavikonetivivs-projects.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
